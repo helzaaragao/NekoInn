d030a035c95eb9e189eed3599517747bc13b5eea
Helza Aragão <helza.batista.aragao@gmail.com>
2025-05-23 12:09:50 -0300

    implementei o envio de email para usuário e adm utilizando EmailJS

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -12,6 +12,13 @@ interface FormData{
     purrPlan: string
 }
 
+const emailjsConfig = {
+    serviceId: import.meta.env.VITE_PUBLIC_EMAILJS_SERVICE_ID || '',
+    templateIdAdm: import.meta.env.VITE_PUBLIC_EMAILJS_TEMPLATE_ID_ADMIN || '',
+    templateIdUser: import.meta.env.VITE_PUBLIC_EMAILJS_TEMPLATE_ID_USER || '',
+    userId: import.meta.env.VITE_PUBLIC_EMAILJS_USER_ID || ''
+}
+
 export function Contact(){
     const [formData, setFormData] = useState<FormData>({
         name: '',
@@ -26,6 +33,7 @@ export function Contact(){
       const [isSubmitting, setIsSubmitting] = useState(false);
       const [submitSuccess, setSubmitSuccess] = useState(false);
       const [submitError, setSubmitError] = useState('');
+      const [isEmailSent, setIsEmailSent] = useState(false)
 
         const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
         const { name, value } = e.target;
@@ -40,66 +48,92 @@ export function Contact(){
         setIsSubmitting(true);
         setSubmitError('');
 
+         if (isSubmitting || isEmailSent) return;
+
         try{
-            //envia o email para mim/a adm
             await emailjs.send(
-                 process.env.REACT_APP_EMAILJS_SERVICE_ID!,
-                process.env.REACT_APP_EMAILJS_TEMPLATE_ID_ADMIN!,
+                emailjsConfig.serviceId,
+                emailjsConfig.templateIdAdm,
                 {
                    ...formData, 
                    to_email: 'helza.batista.aragao@gmail.com',
-                   
-                }
+                   subject: `Novo agendamento no Neko Inn - ${formData.name}`
+                },
+                emailjsConfig.userId
+            );
+            await emailjs.send(
+                    emailjsConfig.serviceId,
+                    emailjsConfig.templateIdUser,
+                  {
+                    ...formData,
+                    to_email: formData.email,
+                    subject: 'Auto-reply: Agendamento concluído! | Neko Inn'
+                  },
+                   emailjsConfig.userId
             )
+            setIsEmailSent(true);
+            setSubmitSuccess(true)
+        } catch(error){
+             console.error('Erro ao enviar email:', error);
+            setSubmitError('Ocorreu um erro ao enviar o formulário. Por favor, tente novamente.');
+        } finally {
+            setIsSubmitting(false);
         }
+  };
+  if(submitSuccess){
+    return(
+         <ContactContainer>
+                <header>
+                    <span>AGENDAMENTO CONFIRMADO!</span>
+                    <h2>Obrigado por reservar conosco!</h2>
+                    <p>Enviamos uma confirmação para {formData.email} com os detalhes do seu agendamento para o plano {formData.purrPlan}.</p>
+                </header>
+            </ContactContainer>
+    )
   }
-
-    // const sendEmail = (e) => {
-    //     
-
-    // }
     return(
         <ContactContainer>
         <header>
             <span>SECURE YOUR CAT'S STAY TODAY</span>
                 <h2>Book Now</h2>
                 <p>Ready to reserve your cat's spot at NekoInn? Our easy-to-use booking system makes it simple to schedule your cat's stay with us. Just select your desired dates, choose from our range of accommodations and services, and complete your reservation in a few easy steps.</p>
+                {submitError && <p className="error-message">{submitError}</p>}
         </header>
-        <FormContact action="" method="post">
+        <FormContact onSubmit={handleSubmit}>
             <div>
                 <label htmlFor="name">FULL NAME</label>
-                <input type="text" id="name" name="name"/>
+                <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required/>
             </div>
             <div>
                 <label htmlFor="email">EMAIL</label>
-                <input type="email" id="email" name="email" />
+                <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required/>
             </div>
             <div>
                 <label htmlFor="phone">PHONE NUMBER</label>
-                <input type="tel" id="phone" name="phone" />
+                <input type="tel" id="phone" name="phone" value={formData.phone} onChange={handleChange} required/>
             </div>
             <div>
                 <label htmlFor="catName">CAT NAME</label>
-                <input type="text" id="catName" name="catName" />
+                <input type="text" id="catName" name="catName" value={formData.catName} onChange={handleChange} required/>
             </div>
             <div>
                 <label htmlFor="date">PICK A DATE</label>
-                <input type="date" id="scredullingDate" name="scredullingDate" />
+                <input type="date" id="scredullingDate" name="scredullingDate" value={formData.scredullingDate} onChange={handleChange} required />
             </div>
             <div>
                 <label htmlFor="numberPets">HOW MANY PETS?</label>
-                <input type="number" id="numberPets" name="numberPets" />
+                <input type="number" id="numberPets" name="numberPets" value={formData.numberPets} onChange={handleChange} required min="1"/>
             </div>
             <div>
                 <label htmlFor="purrPlan">PURR PLAN</label>
-                <select name="purrPlan" id="purrPlan" required>
+                <select name="purrPlan" id="purrPlan" value={formData.purrPlan} onChange={handleChange} required>
                     <option defaultValue="selecione">Selecione</option>
                     <option value="PURR DUCAL">PURR DUCAL</option>
                     <option value="PURR IMPERIAL">PURR IMPERIAL</option>
                     <option value="PURR ROYAL">PURR ROYAL</option>
                 </select>
             </div>
-            <button>BOOK NOW</button>
+            <button type="submit" disabled={isSubmitting}> {isSubmitting ? 'SENDING...' : 'BOOK NOW'}</button>
            
 
         </FormContact>
